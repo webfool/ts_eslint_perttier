a392ffc0a2a93bb266544e8ef295354634434a48
Haowen Liu <haowen.liu@shopee.com>
2020-11-12 17:34:21 +0800

    集成eslint + typescript + prettier 校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,27 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
+  // === ts 配置 ===
+  parser: '@typescript-eslint/parser',
+  parserOptions: {},
   env: {
     browser: true,
     es2021: true,
     node: true
   },
-  // plugins: ['prettier'],
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended', // 使用@typescript-eslint/eslint-plugin的推荐规则
+    /**
+     * 使用 prettier 插件的 recommended 配置，它包括注入
+     * {
+     *    plugins: ['prettier'],
+     *    rules: {
+     *      'prettier/prettier': 'error'
+     *    },
+     *    extends: ['prettier'] // 使用 eslint-config-prettier 关闭 eslint 和 prettier 冲突的规则
+     * }
+     */
+    'plugin:prettier/recommended'
+  ],
   rules: {
-    // 'prettier/prettier': 'error'
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'] // 覆盖 typescript 校验规则
   }
 }
---
+++ src/index2.ts
@@ -0,0 +1,6 @@
+const a = 1
+console.log('abc')
+
+interface A {
+  name: string
+}
